9f24737d0d2762470c9c10c23c1fb33caf86c1f6
Luigi Bertaco Cristofolini <luigibertaco@gmail.com>
2016-02-29 22:24:57 +1100

    iotduino dependency

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 var forEach = require('async-foreach').forEach;
 var request = require('request');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var duino = require( 'iotduino'),
+    pinMode = duino.PinMode, pinState = duino.PinState,
+    pins = duino.Pins, ledPin = pins.GPIO13;
+
 app.use( bodyParser.json() );
 
 var servers = [];
@@ -10,11 +14,17 @@ var servers = [];
 app.post('/registerserver', function (req, res) {
     console.log(req.body);
     servers.push(req.body.url);
-    res.send('Hello World!');
+    res.send('Hello World! with iotduino');
+});
+
+app.get('/toggle', function (req, res){
+    duino.digitalWrite( ledPin, !duino.digitalRead( ledPin));
+    res.send('Actual value for pin 13: ' + duino.digitalRead( ledPin));
 });
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
+    duino.pinMode( ledPin, pinMode.OUTPUT);
     setInterval(sendToServers, 1000);
 });
 
