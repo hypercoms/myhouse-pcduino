d3e38011c3bc6a9ac5e41efb75428dcb62b6c1c9
Luigi Bertaco Cristofolini <luigibertaco@gmail.com>
2016-02-29 22:38:03 +1100

    leds 2,3 and 4

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var bodyParser = require('body-parser');
 var duino = require( 'iotduino'),
     pinMode = duino.PinMode, pinState = duino.PinState,
     pins = duino.Pins, ledPin = pins.GPIO13;
+var led2 = pins.GPIO2;
+var led3 = pins.GPIO3;
+var led4 = pins.GPIO4;
 
 app.use( bodyParser.json() );
 
@@ -20,6 +23,21 @@ app.post('/registerserver', function (req, res) {
 app.get('/toggle', function (req, res){
     duino.digitalWrite( ledPin, !duino.digitalRead( ledPin));
     res.send('Actual value for pin 13: ' + duino.digitalRead( ledPin));
+});
+
+app.get('/toggle2', function (req, res){
+    duino.digitalWrite( led2, !duino.digitalRead( led2));
+    res.send('Actual value for pin 2: ' + duino.digitalRead( led2));
+});
+
+app.get('/toggle3', function (req, res){
+    duino.digitalWrite( led3, !duino.digitalRead( led3 ));
+    res.send('Actual value for pin 3: ' + duino.digitalRead( led3 ));
+});
+
+app.get('/toggle4', function (req, res){
+    duino.digitalWrite( led4, !duino.digitalRead( led4));
+    res.send('Actual value for pin 4: ' + duino.digitalRead( led4));
 });
 
 app.listen(3000, function () {
